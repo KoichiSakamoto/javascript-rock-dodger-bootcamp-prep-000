b8bd6c7e382dab2827be9b8469e5d763b9126b8f
Isaac Fiore <isaac.fiore@mail.mcgill.ca>
2018-05-07 02:03:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -90,6 +90,9 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
+     if (checkCollision(rock)) {
+       endGame()
+     }
      var top = 0
 
      function step() {
