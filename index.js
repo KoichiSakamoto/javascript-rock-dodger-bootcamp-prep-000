089fca985a5debead744e5113f51c599355a57b7
Isaac Fiore <isaac.fiore@mail.mcgill.ca>
2018-05-07 02:14:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -171,11 +171,11 @@ function moveDodgerLeft() {
    * This function should move DODGER to the left
    * (mabye 4 pixels?). Use window.requestAnimationFrame()!
    */
-   var leftNumbers = DODGER.style.left.replace('px', '')
+   var leftNumbers = dodger.style.left.replace('px', '')
    var left = parseInt(leftNumbers, 10)
 
    if (left > 0) {
-     dodger.style.left = `${left - 2}px`
+     dodger.style.left = `${left - 1}px`
    }
 }
 
