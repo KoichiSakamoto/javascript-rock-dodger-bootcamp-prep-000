65fd760e0023bcf0717221637aea2f0ffebfd81f
Isaac Fiore <isaac.fiore@mail.mcgill.ca>
2018-05-07 01:50:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -138,7 +138,7 @@ function endGame() {
   clearInterval(gameInterval)
 
   var game = GAME
-  console.log(`${document.querySelector('rock')}`)
+  console.log(`${document.querySelector('.rock')}`)
 }
 
 function moveDodger(e) {
