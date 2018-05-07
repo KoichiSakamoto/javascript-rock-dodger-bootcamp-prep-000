f9b08467a16585ff62d14af1d982dab8e7947414
Isaac Fiore <isaac.fiore@mail.mcgill.ca>
2018-05-07 02:11:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -155,6 +155,7 @@ function moveDodger(e) {
    * And be sure to use the functions declared below!
    */
    if (e.which == 37) {
+     e.preventDefault()
      moveDodgerLeft()
    }
    else if (e.which == 39) {
