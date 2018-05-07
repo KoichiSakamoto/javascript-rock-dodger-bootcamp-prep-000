0d9429abb8c4adf323b58d8ebe13c1ed60e23489
Isaac Fiore <isaac.fiore@mail.mcgill.ca>
2018-05-07 00:58:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,13 +36,13 @@ function checkCollision(rock) {
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
     const rockRightEdge = positionToInteger(rock.style.right);
 
-    if (rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerLeftEdge) {
+    if (rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) {
       return true
     }
-    else if (rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerRightEdge) {
+    else if (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) {
       return true
     }
-    else if (rockLeftEdge < dodgerRightEdge && rockRightEdge > dodgerRightEdge) {
+    else if (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge) {
       return true
     }
               /**
@@ -73,7 +73,7 @@ function createRock(x) {
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-   
+
 
   /**
    * This function moves the rock. (2 pixels at a time
