1f275bb22f5f6d7768991c972a1442bc38d0dda2
Isaac Fiore <isaac.fiore@mail.mcgill.ca>
2018-05-07 02:04:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function createRock(x) {
    * This function moves the rock. (2 pixels at a time
    * seems like a good pace.)
    */
-  function moveRock(rock) {
+  function moveRock() {
     // implement me!
     // (use the comments below to guide you!)
     /**
@@ -120,7 +120,7 @@ function createRock(x) {
   }
 
   // We should kick of the animation of the rock around here
-  moveRock(rock)
+  moveRock()
   // Add the rock to ROCKS so that we can remove all rocks
   // when there's a collision
   ROCKS.push(rock)
